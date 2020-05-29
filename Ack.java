e5d539aca9b7acb356724910b9433379d1d4246e
Gabriel <gabriel.m.alves13@gmail.com>
2020-05-29 01:38:35 -0300

    second commit

---
+++ Ack.java
@@ -0,0 +1,15 @@
+import java.io.Serializable;
+
+public class Ack implements Serializable {
+	
+	public int id;
+	
+	public Ack(int id) {
+		super();
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+}
---
+++ Packet.java
@@ -0,0 +1,25 @@
+import java.io.Serializable;
+
+public class Packet implements Serializable {
+	
+	public int id;
+	public String msg;
+	
+	public Packet(int id, String msg) {
+		super();
+		this.id = id;
+		this.msg = msg;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int setId) { 
+		this.id = setId; 
+	}
+	
+	public String getMsg() {
+		return msg;
+	}
+}
